0d4ef9f1e0527b2ee290d29e8322fda811a5d495
smartteddy81 <smartteddykb81@gmail.com>
2018-12-18 11:23:49 +0100

    backup data

--- GitBackup.sh
+++ GitBackup.sh
@@ -2,4 +2,12 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 export DISPLAY=:0.0
 sleep 5
-git status
+git config --global user.email "smartteddykb81@gmail.com"
+git config --global user.name "smartteddy81"
+git add .
+git commit -m "backup data"
+git push
+sleep 1
+echo | "smartteddy81"
+sleep 2
+echo | "smartteddy_kb81"
